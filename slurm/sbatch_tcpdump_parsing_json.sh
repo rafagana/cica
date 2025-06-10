267863d26b2c42d4c33ca1a7ad53c7b67d3c374d
rafagana <rafagana@gmail.com>
2025-06-10 10:50:59 +0200

    slurm json

--- slurm/sbatch_tcpdump_parsing_json.sh
+++ slurm/sbatch_tcpdump_parsing_json.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks=1                  # Number of tasks (1 CPU)
 #SBATCH --cpus-per-task=1           # Number of CPU cores per task
-#SBATCH --mem=18G                   # Memory allocation (18GB)
+#SBATCH --mem=64G                   # Memory allocation (64GB)
 #SBATCH --time=72:00:00             # Maximum runtime (3 days)
 
 # Load system-wide environment variables
@@ -18,7 +18,7 @@ source /home/LQL3227/phd/bin/activate
 cd /home/LQL3227/phd/data-engineering
 
 # Run the Python script
-python tcpdump_parsing_json_cica.py
+python3 tcpdump_parsing_json_cica.py
 
 # Deactivate the virtual environment (optional, but good practice)
 deactivate
